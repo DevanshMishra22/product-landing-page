d378758c6710190a6c3a144a88c4263447018c68
DevanshMishra22 <mishradevansh2233@gmail.com>
2023-07-11 03:09:13 +0530

    Add files via upload

---
+++ ham.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const navlist = document.getElementById("navlist");
+
+hamburger.addEventListener("click",()=>{
+
+navlist.classList.toggle("navlist-active")
+})
